bc31480d95a59345074840502ec7994b63e7eb73
Damien <damien.kan@gmail.com>
2012-08-02 15:52:12 -0700

    HEROKU PUSH

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExecStore::Application.config.secret_token = '2d2b0ad0693ff88af06d98f78d725d05f67c0f475a79fc8f6c25f43ffc770bd61ecc7e08d4b3b0cf67426cc65130c2646b1353aecf54c16d1c41e6138f157c6b'
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,1 @@
+Stripe.api_key = "AsaoaBq2yJkTfe7pWQQJz72KaDQjUx7N"

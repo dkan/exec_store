b913d4fe71b1372d895a17e90f296fcf9df0242a
Damien <damien.kan@gmail.com>
2012-08-08 21:22:01 -0700

    2 hoodie test

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ end
 
 module ExecStore
   class Application < Rails::Application
-      HOODIE_LIMIT = 30
+      HOODIE_LIMIT = 2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

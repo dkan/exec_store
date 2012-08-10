33e9df844c2e328e6c1de392d87912fd608182fe
Damien <damien.kan@gmail.com>
2012-08-09 20:54:57 -0700

    ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+  
+  def ensure_domain
+    if Rails.env.production?
+        if !request.ssl?
+            redirect_to "https://store.iamexec.com#{request.fullpath}", :status => 301
+        end
+    end
+  end
+  
 end
